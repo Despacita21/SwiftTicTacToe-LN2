52789df4a950b9554c56f0df00001b342a8ad5a9
Lauren Najera <lan20a@acu.edu>
2022-12-12 22:46:55 -0600

    Initial Commit

---
+++ SwiftTicTacToe-LN2/SwiftTicTacToe_LN2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTicTacToe_LN2App.swift
+//  SwiftTicTacToe-LN2
+//
+//  Created by Lauren Najera on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTicTacToe_LN2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
